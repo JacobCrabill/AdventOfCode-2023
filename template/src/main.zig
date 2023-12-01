7ef1a6c1d86ebac015bf655de00cad5ba3728584
Jacob Crabill <jacob.crabill@shield.ai>
2023-12-01 14:03:20 -0800

    Improve template; add placeholders for Part2 inputs
    Turns out you sometimes need different inputs for part 2

--- template/src/main.zig
+++ template/src/main.zig
@@ -7,55 +7,61 @@ const ArrayList = std.ArrayList;
 const GPA = std.heap.GeneralPurposeAllocator;
 const ns2sec = utils.ns2sec;
 
-const test_input = Data.test_input;
-const input = Data.input;
-
-const log = std.log.scoped(.aoc);
+const log = std.log.scoped(.AoC);
 
 pub fn main() !void {
     var gpa = GPA(.{}){};
+    defer _ = gpa.deinit(); // Performs leak checking
     var alloc = gpa.allocator();
 
     var T = try std.time.Timer.start();
 
-    var res1 = try part1(input, alloc);
-    log.info("Part1: {d}", .{res1});
+    const res1 = try part1(Data.input, alloc);
+    log.info("Part 1 answer: {d}", .{res1});
     log.info("Part 1 took {d:.6}s", .{ns2sec(T.lap())});
 
-    var res2 = try part2(input, alloc);
-    log.info("Part2: {d}", .{res2});
+    const res2 = try part2(Data.input, alloc);
+    log.info("Part 2 answer: {d}", .{res2});
     log.info("Part 2 took {d:.6}s", .{ns2sec(T.lap())});
 }
 
 // ------------ Tests ------------
 
 test "part1 test input" {
-    log.warn(" -- Running Tests --", .{}); // First line of test output is confusingly prepended with `run test: error:`
+    // The first line of test output is confusingly prepended with `run test: error:`
+    // Adding an extra initial line makes the output easier to parse
+    log.warn(" -- Running Tests --", .{});
 
     const answer: usize = 0;
+
     var alloc = std.testing.allocator;
-    var res = try part1(test_input, alloc);
+    var res = try part1(Data.test_input, alloc);
     log.warn("[Test] Part 1: {d}", .{res});
     try std.testing.expect(res == answer);
 }
 
 test "part2 test input" {
     const answer: usize = 0;
+
     var alloc = std.testing.allocator;
-    var res = try part2(test_input, alloc);
+    var res = try part2(Data.test_input, alloc);
     log.warn("[Test] Part 2: {d}", .{res});
     try std.testing.expect(res == answer);
 }
 
 // ------------ Part 1 Solution ------------
 
-pub fn part1(_: []const u8, _: Allocator) !usize {
+pub fn part1(input: []const u8, alloc: Allocator) !usize {
+    _ = alloc;
+    _ = input;
     return 0;
 }
 
 // ------------ Part 2 Solution ------------
 
-pub fn part2(_: []const u8, _: Allocator) !usize {
+pub fn part2(input: []const u8, alloc: Allocator) !usize {
+    _ = alloc;
+    _ = input;
     return 0;
 }
 
